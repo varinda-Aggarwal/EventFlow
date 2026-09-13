ffbf9276f64a46986575ff8b5232afefad5e1063
varinda <varindaptrn@gmail.com>
2026-09-13 14:56:11 +0530

    feat: simplify registration validation now that name and email come from the account

--- server/middleware/validators.js
+++ server/middleware/validators.js
@@ -128,17 +128,6 @@ const updateEventValidation = [
 ];
 
 const registrationValidation = [
-  body('name')
-    .trim()
-    .notEmpty().withMessage('Name is required')
-    .isLength({ min: 2, max: 50 }).withMessage('Name must be between 2 and 50 characters'),
-
-  body('email')
-    .trim()
-    .notEmpty().withMessage('Email is required')
-    .isEmail().withMessage('Enter a valid email address')
-    .normalizeEmail(),
-
   body('phone')
     .trim()
     .notEmpty().withMessage('Phone number is required')
